a4971833d63f17d35312c5051728bacc46ae1da9
Feyzo <feyzullahdaldal@gmail.com>
2015-12-22 12:36:03 +0000

    Initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './public/javascripts/app.js',
+  output: {
+    path: __dirname,
+    filename: './public/build/bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loader: 'style!css'
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  }
+};
